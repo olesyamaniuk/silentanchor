64a617972acc183da80b0432cf30b908adaa50e9
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2026-04-29 19:46:09 +0300

    3

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".class-navigation-list");
-  const headerRef = document.querySelector(".class-header");
-  const navigationRef = document.querySelector(".class-navigation");
+  const navListRef = document.querySelector(".o-d-navigation-list");
+  const headerRef = document.querySelector(".o-d-header");
+  const navigationRef = document.querySelector(".o-d-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigationRef.classList.toggle("is-open");
   });
 
-  document.querySelectorAll(".class-navigation-link").forEach((n) =>
+  document.querySelectorAll(".o-d-navigation-link").forEach((n) =>
     n.addEventListener("click", () => {
       navListRef.classList.remove("is-open");
       menuBtnRef.classList.remove("is-open");
@@ -20,17 +20,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }),
   );
 });
-function toggleAnswer(element) {
-  const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".class-toggle-button img");
-
-  answer.classList.toggle("open");
-
-  if (answer.classList.contains("open")) {
-    buttonImg.src = "/img/Minus.png";
-    buttonImg.classList.add("rotated");
-  } else {
-    buttonImg.src = "/img/Add.png";
-    buttonImg.classList.remove("rotated");
-  }
-}
